086a36165f6e29072af3c7f024d0a93a89cec635
amruthavelumuri <amruthavelumuri@gmail.com>
2022-06-03 20:33:10 +0530

    my commit

--- src/main/java/MavenTest.java
+++ src/main/java/MavenTest.java
@@ -4,6 +4,7 @@ public class MavenTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sample Maven");
+		System.out.println("Sample");
 
 	}
 
